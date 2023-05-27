c7774795075b0957e2941e1ef7cf1a11443e61dc
Angelillo15 <contact@angelillo15.es>
2023-05-27 14:37:39 +0200

    Load libs on startup

--- BlockTimerRemover-Bukkit/src/main/java/es/angelillo15/btr/BTRLoader.java
+++ BlockTimerRemover-Bukkit/src/main/java/es/angelillo15/btr/BTRLoader.java
@@ -1,12 +1,14 @@
 package es.angelillo15.btr;
 
 import es.angelillo15.btr.api.utils.Metrics;
+import es.angelillo15.btr.utils.LibsLoader;
 import io.papermc.lib.PaperLib;
 
-public class BTRLoader extends BlockTtimerRemover {
+public class BTRLoader extends BlockTimerRemover {
     @Override
     public void onEnable() {
         drawLogo();
+        LibsLoader.load();
         loadConfig();
         registerCommands();
         registerListeners();
