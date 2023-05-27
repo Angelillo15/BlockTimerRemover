5382f77ae1c241ddd6aa1f742d87ded00284e47f
Angelillo15 <contact@angelillo15.es>
2023-05-27 14:19:44 +0200

    Added loader methods

--- BlockTimerRemover-Bukkit/src/main/java/es/angelillo15/btr/BTRLoader.java
+++ BlockTimerRemover-Bukkit/src/main/java/es/angelillo15/btr/BTRLoader.java
@@ -1,4 +1,22 @@
 package es.angelillo15.btr;
 
+import es.angelillo15.btr.api.utils.Metrics;
+import io.papermc.lib.PaperLib;
+
 public class BTRLoader extends BlockTtimerRemover {
+    @Override
+    public void onEnable() {
+        drawLogo();
+        loadConfig();
+        registerCommands();
+        registerListeners();
+        PaperLib.suggestPaper(this);
+        new Metrics(this, 18583);
+    }
+
+    @Override
+    public void onDisable() {
+        unregisterCommands();
+        unregisterListeners();
+    }
 }
